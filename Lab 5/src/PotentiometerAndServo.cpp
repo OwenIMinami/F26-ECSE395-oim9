8d68cc030863463f7b0720f16781987d3816f037
Owen Minami <oim9@case.edu>
2026-09-25 15:15:04 -0400

    Enhance PotentiometerAndServo.cpp with comments
    Added comments for clarity and understanding of the code.

--- Lab 5/src/PotentiometerAndServo.cpp
+++ Lab 5/src/PotentiometerAndServo.cpp
@@ -3,11 +3,11 @@
 
 Servo myServo;
 const int servoPin = A0; // declare the pin number for the servo motor oim9
-const int sensorPin = A2;
+const int sensorPin = A2; // declare the pin number for the potentiometer oim9
 
-int pulseWidth;
-int sensor;
-int angle;
+int pulseWidth; // initiate pulsewidth varaible oim9
+int sensor; // initiate sensor variable oim9
+int angle; // initiate angle variable oim9
 
 // Define the minimum and maximum pulse widths for the servo
 const int minPulseWidth = 500; // 0.5 ms
@@ -15,25 +15,22 @@ const int maxPulseWidth = 2500; // 2.5 ms
 
 void setup() {
 
-    Serial.begin(115200);
-    myServo.attach(servoPin, minPulseWidth, maxPulseWidth);
+    Serial.begin(115200); // set baud rate oim9
+    myServo.attach(servoPin, minPulseWidth, maxPulseWidth); // attach the servo to the right pin and use the right pulsewidths
 
-    myServo.setPeriodHertz(50); 
-    Serial.println("hello");
+    myServo.setPeriodHertz(50); //set the PWM frequency oim9
 }
 
 
 void loop() {
 
-    sensor = analogRead(sensorPin);
+    sensor = analogRead(sensorPin); // read potentiometer signal oim9
 
-    //  --- SECTION 1: Make a Random Angle Between 0 to 180 ---
-    angle = sensor / 1023.0 * 180; // Map the sensor value to an angle between 0 and 180
+    angle = sensor / 1023.0 * 180; // map potentiometer signal to angle oim9
 
-    Serial.println(angle);
+    Serial.println(angle); // print the angle oim9
 
-    // ---SECTION 2: Map Pulse Width with Angle
-    pulseWidth = map(angle, 0, 180, minPulseWidth, maxPulseWidth); // set pulse width based on the sensor reading
+    pulseWidth = map(angle, 0, 180, minPulseWidth, maxPulseWidth); // set pulse width based on the sensor reading oim9
     myServo.writeMicroseconds(pulseWidth); // write pulse width to servo oim9
-    
+
 }
